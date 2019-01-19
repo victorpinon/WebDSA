55193bb95d94c05094ebc11def4f7bc67024bc0f
victorpinon <victorpinonrattia@hotmail.com>
2019-01-19 15:03:52 +0100

    detallets

--- scripts/indexJs.js
+++ scripts/indexJs.js
@@ -21,13 +21,42 @@ $(document).ready(function() {
 	                if (data.code == 0){
 	                	alert("Wrong password");
 	                } else if (data.code == 1) {
-	                	alert("User is banned");
+	                	alert("This user is banned");
 	                } else if (data.code == 2) {
 	                	localStorage.setItem( "userName",  $("#user").val());
 	                	window.open("scoreboardNormalUser.html", "_self");  
 	                } else if (data.code == 3) {
 	                	localStorage.setItem( "userName",  $("#user").val());
 	                	window.open("adminUserList.html", "_self");  	
+	                } else {
+	                	alert("Unknown error");
+	                }
+	            }
+        	});
+		} else{
+			alert("Insert a User Name and a Password please");
+		}
+	});
+
+	$("#register").click(function(){
+		if($("#password").val() != "" && $("#user").val()!=  ""){
+			$.ajax({
+			 	contentType: "application/json;charset=utf-8",
+	            type: "POST",
+	            dataType: "json",	
+	            url: 'http://147.83.7.203:8080/APIGame/user/register',
+	            data: JSON.stringify({
+					userName: $("#user").val(),
+	              	password: $("#password").val()
+				}),
+	            success: function(data)
+	            {
+					if (data.code == 1) {
+	                	alert("Registered succesfully, please log in");
+	                } else if (data.code == 2) {
+	                	alert("User already exists");
+	                } else if (data.code == 3) {
+	                	alert("Unknown error"); 	
 	                } else {
 	                	alert("Unknown error");
 	                }
@@ -94,6 +123,11 @@ $(document).ready(function() {
 	$("#goScoreboard").click(function(){
 		localStorage.removeItem("userInfoName");
 		window.open("scoreboard.html", "_self");
+	});
+
+	$("#goAllGames").click(function(){
+		localStorage.removeItem("userInfoName");
+		window.open("allGames.html", "_self");
 	});
 
 	/*
@@ -364,11 +398,90 @@ $(document).ready(function() {
 	                	$("#userGameList").append("<tr> <td><b>"+data[i].nameGame+"</b></td> <td>"+data[i].healthPoints+"  <span class='glyphicon glyphicon-heart'></span></td> <td>"+completed+"</td> <td>"+data[i].gameLength+"</td></tr>");
                 	}
             	} else {
-            		$("#userGameList").html("<h4><b>This user has not played any games</b></h4>");
+            		$("#userGameList").html("<h4><b>You have not played any games</b></h4>");
             	}
                 
             }
         });
+
+	}
+
+	/*
+	*********************************************************************************
+									allGames.html
+	*********************************************************************************
+	*/
+	if(window.document.title == "All Games"){
+		$("#navBarName").text(localStorage.getItem("userName"));
+
+		var list = [];
+
+		$.ajax({
+		 	contentType: "application/json;charset=utf-8",
+            type: "GET",
+            url: 'http://147.83.7.203:8080/APIGame/user/loadUsers',
+            success: function(data)
+            {
+            	var i;
+                for (i = 0; i < data.length; i++) {
+            		let name = data[i].userName;
+            		$.ajax({
+					 	contentType: "application/json;charset=utf-8",
+			            type: "GET",
+			            url: 'http://147.83.7.203:8080/APIGame/game/gameList/'+name,
+			            success: function(data)
+			            {
+			            	var j;
+			                for (j = 0; j < data.length; j++) {
+
+			                	let completed = false;
+			                	if(data[j].isCompleted == 1){
+			                		completed = true;
+			                	}
+
+			                	list.push({
+			                		"nameGame": data[j].nameGame,
+			                		"userName": name,
+			                		"gameLength": data[j].gameLength,
+			                		"healthPoints": data[j].healthPoints,
+			                		"isCompleted": completed
+			                	});
+			                }
+			            }
+			        });
+                }
+            }
+        });
+
+        setTimeout(() => {
+			renderSortedTable(list)
+		}, 1300)
+
+		function sorter(item1, item2) {
+			if(item1.gameLength > item2.gameLength) {
+				return 1;
+			} 
+
+			if(item1.gameLength < item2.gameLength) {
+				return -1;
+			}
+
+			return 0;
+		}
+
+		// Podemos delegar/abstraer el render a un metodo separado
+		function renderSortedTable(list) {
+
+			// Ordenar lista
+			let sortedList = list.sort(sorter)
+			//console.log(sortedList)
+			for(var j = 0; j < list.length; j++){
+				
+				$("#gameList").append("<tr> <td><b>"+list[j].userName+"</b></td><td>"+list[j].nameGame+"</td> <td>"+list[j].gameLength+"</td> <td>"+list[j].healthPoints+"  <span class='glyphicon glyphicon-heart'></span></td> <td>"+list[j].isCompleted+"</td></tr>");
+				
+			}
+
+		}
 
 	}
 
